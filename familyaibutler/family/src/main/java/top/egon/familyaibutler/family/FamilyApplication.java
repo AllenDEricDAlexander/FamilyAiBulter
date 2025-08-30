c91e9da801785d7e52b10bea008efa9569b2cdfd
luofu <mqa010225@163.com>
2025-08-30 13:17:45 +0800

    feat(family): 更新应用配置和依赖
    - 修改应用名称为 family-base-model
    - 更新 Nacos配置服务器地址和命名空间- 添加 Spring Cloud Discovery 客户端支持
    - 增加 Spring Boot Actuator 依赖

--- familyaibutler/family/src/main/java/top/egon/familyaibutler/family/FamilyApplication.java
+++ familyaibutler/family/src/main/java/top/egon/familyaibutler/family/FamilyApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
@@ -22,6 +23,7 @@ import java.net.UnknownHostException;
  */
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("top.egon.familyaibutler.family.mapper")
 @ComponentScan(basePackages = {"top.egon.familyaibutler.family", "top.egon.familyaibutler.common"})
 public class FamilyApplication {
